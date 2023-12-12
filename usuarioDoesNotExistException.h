5c2415fd6ca1c70421eab868ac08532ab6ee5d77
pwwlwhc <151556263+pwwlwhc@users.noreply.github.com>
2023-12-11 23:33:08 -0300

    Add files via upload

---
+++ usuarioDoesNotExistException.h
@@ -0,0 +1,14 @@
+#ifndef BUNDASUJA_USUARIODOESNOTEXISTEXCEPTION_H
+#define BUNDASUJA_USUARIODOESNOTEXISTEXCEPTION_H
+
+
+#include <stdexcept>
+
+class usuarioDoesNotExistException : std::invalid_argument{
+public:
+    const char *what() const _GLIBCXX_TXN_SAFE_DYN _GLIBCXX_NOTHROW override;
+    explicit usuarioDoesNotExistException(const std::string &arg);
+};
+
+
+#endif //BUNDASUJA_USUARIODOESNOTEXISTEXCEPTION_H
